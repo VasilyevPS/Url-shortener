d05e2bc92f319764e5ccba6e50d2370374d520fa
Pavel Vasilyev <pavel.vas@mail.ru>
2024-04-09 23:11:31 +0300

    Add GitHub action and test coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-	java
-	id ("checkstyle")
+	application
+	checkstyle
+	jacoco
 	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -28,6 +29,16 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+application {
+	mainClass = "vasilyevps.urlshortener.UrlShortenerApplication.java"
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required = true
+	}
 }
 
 tasks.withType<Test> {
