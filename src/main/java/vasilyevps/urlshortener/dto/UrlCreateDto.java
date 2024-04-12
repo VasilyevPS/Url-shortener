2d0be3194a3fc50b1c1af1a828aa17e8dc52c6fa
Pavel Vasilyev <pavel.vas@mail.ru>
2024-04-12 22:43:39 +0300

    Fix Dto classes

--- src/main/java/vasilyevps/urlshortener/dto/UrlCreateDto.java
+++ src/main/java/vasilyevps/urlshortener/dto/UrlCreateDto.java
@@ -1,10 +1,15 @@
 package vasilyevps.urlshortener.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UrlCreateDto {
+    @NotBlank
     private String url;
 }
--- src/main/java/vasilyevps/urlshortener/dto/UrlDto.java
+++ src/main/java/vasilyevps/urlshortener/dto/UrlDto.java
@@ -1,10 +1,12 @@
 package vasilyevps.urlshortener.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UrlDto {
     private String key;
     private String shortUrl;
