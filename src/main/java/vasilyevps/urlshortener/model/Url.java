ca717e24a8ff105037af9aa3e1a58a43ee6e18b1
Pavel Vasilyev <pavel.vas@mail.ru>
2024-04-12 13:21:36 +0300

    Add Url model

---
+++ src/main/java/vasilyevps/urlshortener/model/Url.java
@@ -0,0 +1,49 @@
+package vasilyevps.urlshortener.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static jakarta.persistence.TemporalType.TIMESTAMP;
+
+@Entity
+@Table(name = "urls", indexes = @Index(columnList = "urlKey", name = "urlKeyIndex"))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @JsonIgnore
+    private long id;
+
+    @Column(unique = true)
+    private String urlKey;
+
+    @Column(unique = true)
+    private String shortUrl;
+
+    @Column(unique = true)
+    @NotBlank
+    private String longUrl;
+
+    @CreationTimestamp
+    @Temporal(TIMESTAMP)
+    private Date createdAt;
+}
